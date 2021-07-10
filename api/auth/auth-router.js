4cc52ab2edfa51bca716b18a482596ae776333c4
Lakendria <kendra939@gmail.com>
2021-07-10 12:44:30 -0400

    finished auth-router

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const jwt = require('jsonwebtoken');
 
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
@@ -14,6 +15,19 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "role_name": "angel"
     }
    */
+    const credentials = req.body;
+
+    try {
+      const hash = bcryptjs.hashSync(credentials.password, 10);
+      credentials.password = hash;
+  
+      const user = await Users.add(credentials);
+      const token = generateToken(user);
+      res.status(201).json({ data: user, token });
+    } catch (err) {
+      console.log(err);
+      next({ apiCode: 500, apiMessage: 'error saving new user', ...err });
+    }
 });
 
 
@@ -37,6 +51,35 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
       "role_name": "admin" // the role of the authenticated user
     }
    */
+    const { username, password } = req.body;
+
+    try {
+  
+      const [user] = await Users.findBy({ username: username });
+      if (user && bcryptjs.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: 'welcome to the api', token: token });
+      } else {
+        next({ apiCode: 401, apiMessage: 'invalid credentials' });
+      }
+    } catch (err) {
+      next({ apiCode: 500, apiMessage: 'db error logging in', ...err });
+    }
 });
+function generateToken(user) {
+
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    rolename: user.rolename
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+
+  const token = jwt.sign(payload, JWT_SECRET, options);
+
+  return token;
+}
 
 module.exports = router;
